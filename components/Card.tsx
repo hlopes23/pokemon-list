66c053487ab63ee101692f67e1f2844f27e90275
mindera teste <henrique.silva.lopes@outlook.pt>
2025-06-30 15:23:25 +0100

    feat: enhance Card component to highlight search terms in Pokemon names

--- components/Card.tsx
+++ components/Card.tsx
@@ -1,11 +1,24 @@
 'use client';
 
 import Image from "next/image";
-import  {PokemonGridDisplay}  from "../interfaces/Pokemon";
+import { useSearch } from "../context/SearchContext";
+import { PokemonGridDisplay } from "../interfaces/Pokemon";
+
+export default function Card({ pokemon }: { pokemon: PokemonGridDisplay }) {
+  const { search } = useSearch();
 
 
-
-export default function Card( {pokemon} : {pokemon: PokemonGridDisplay}) {
+  function highlightName(name: string, search: string) {
+    if (!search) return name;
+    const regex = new RegExp(`(${search})`, "ig");
+    return name.split(regex).map((part, i) =>
+      part.toLowerCase() === search.toLowerCase() ? (
+        <span key={i} className="bg-green-500 rounded px-1">{part}</span>
+      ) : (
+        part
+      )
+    );
+  }
 
   return (
     <div className="bg-white/20 h-[280px] w-[80%] border border-white/30 rounded-lg shadow-lg p-4">
@@ -17,8 +30,8 @@ export default function Card( {pokemon} : {pokemon: PokemonGridDisplay}) {
         height={150}
         priority= {false}
        className="mx-auto mb-3 h-[60%]"></Image>
-      <div className=" text-lg p-4 text-center capitalize bg-white/20 rounded-md w-full mx-auto">
-        <p>{pokemon.name}</p>
+      <div className="text-lg p-4 text-center capitalize bg-white/20 rounded-md w-full mx-auto">
+        <p>{highlightName(pokemon.name, search)}</p>
       </div>
     </div>
     </div>
