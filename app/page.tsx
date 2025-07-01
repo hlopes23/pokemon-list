332870b777e78be2bc04e12d2d0d0f5f5c4efad6
mindera teste <henrique.silva.lopes@outlook.pt>
2025-07-01 17:16:02 +0100

    fix: remove Cardboard import and update Home component to fetch Pokemon data

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,17 @@
-import Cardboard from "../components/Cardboard";
+
 import PokemonInfo from "../components/PokemonInfo";
 import getPokemons from "../utils/pokemon-data";
 import Image from "next/image";
 
-
+import { getPokemonByName } from "../utils/pokemon-data";
 
 export default async function Home() {
 
   const pokemons = await getPokemons(); 
   console.log(pokemons);
+
+  const pokemon = await getPokemonByName("pikachu");
+  console.log(pokemon);
 
   return (
   <PokemonInfo/>
